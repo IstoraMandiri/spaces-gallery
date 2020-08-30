f4883bc871be2d77ca27a1bc3c4cdbd8e3274db2
Alex Shortt <alexander.shortt@gmail.com>
2020-08-30 16:32:42 -0700

    update pipeline to point to spaces s3 bucket

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -10,10 +10,10 @@ DESTINATION_FILE="src/models/${FILE}.tsx"
 gltf-pipeline -i "${STAGED_FILE}" -o "${STAGED_FILE}" -d
 gltfjsx "${STAGED_FILE}" "${DESTINATION_FILE}" --draco --precision 6 --types
 
-aws s3 cp "${STAGED_FOLDER}" "s3://awge-assets/models/awge-space/${NAME}" --recursive --profile awge
+aws s3 cp "${STAGED_FOLDER}" "s3://spaces-gallery-assets/models/${NAME}" --recursive
 
 rm -r "${STAGED_FOLDER}"
 
 echo -e "\nDON'T FORGET TO CHANGE THE LINK INSIDE THE COMPONENT"
 echo -e "COMPONENT LOCATION: src/models/${FILE}.tsx"
-echo -e "CHANGE '/${FILE}.gltf' to 'https://dwvo2npct47gg.cloudfront.net/models/awge-space/${NAME}/${FILE}.gltf'\n"
+echo -e "CHANGE '/${FILE}.gltf' to 'https://d27rt3a60hh1lx.cloudfront.net/models/${NAME}/${FILE}.gltf'\n"
