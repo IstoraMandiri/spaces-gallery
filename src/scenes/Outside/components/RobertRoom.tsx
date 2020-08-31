3e8a0f7b20e0be2c09b0ab1f06a2e5d7556208b5
Alex Shortt <alexander.shortt@gmail.com>
2020-08-31 01:02:14 -0700

    finish robert room

--- src/scenes/Outside/components/RobertRoom.tsx
+++ src/scenes/Outside/components/RobertRoom.tsx
@@ -3,31 +3,45 @@ import FramedImage from "three-components/FramedImage";
 
 const RobertRoom = () => {
   return (
-    <group rotation={[0, Math.PI + Math.PI / 6, 0]}>
-      <group position={[0, 0, 0]}>
-        <mesh>
-          <boxBufferGeometry attach="geometry" args={[0.1, 0.1, 0.1]} />
-          <meshStandardMaterial attach="material" color="red" />
-        </mesh>
+    <group rotation={[0, Math.PI / 3, 0]}>
+      <group position={[0, 5, -45]}>
+        <FramedImage
+          src="https://d27rt3a60hh1lx.cloudfront.net/content/opening/robert/getsusall.jpg"
+          ratio={[1, 1]}
+          sizeScale={5}
+          position={[8.5, 0, 8]}
+          rotation={[0, -Math.PI / 2, 0]}
+          floating
+        />
         <FramedImage
           src="https://d27rt3a60hh1lx.cloudfront.net/content/opening/robert/cottoncandy.jpg"
           ratio={[1, 1]}
           sizeScale={5}
-          position={[4, 2.5, 0]}
+          position={[6.5, 0, 0]}
+          rotation={[0, -Math.PI / 3, 0]}
+          floating
+        />
+        <FramedImage
+          src="https://d27rt3a60hh1lx.cloudfront.net/content/opening/robert/rapture.jpg"
+          ratio={[2, 2.5]}
+          sizeScale={5}
+          position={[0, 0, -4]}
           floating
         />
         <FramedImage
           src="https://d27rt3a60hh1lx.cloudfront.net/content/opening/robert/dontgoin.jpg"
           ratio={[1, 1]}
           sizeScale={5}
-          position={[-4, 2.5, 0]}
+          position={[-6.5, 0, 0]}
+          rotation={[0, Math.PI / 3, 0]}
           floating
         />
         <FramedImage
-          src="https://d27rt3a60hh1lx.cloudfront.net/content/opening/robert/getsusall.jpg"
+          src="https://d27rt3a60hh1lx.cloudfront.net/content/opening/robert/spaceship.jpg"
           ratio={[1, 1]}
           sizeScale={5}
-          position={[0, 2.5, 0]}
+          position={[-8.5, 0, 8]}
+          rotation={[0, Math.PI / 2, 0]}
           floating
         />
       </group>
