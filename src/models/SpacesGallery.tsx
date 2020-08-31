8c4f187e191ca8f1d3279cc3a2a1aa7c4f1cdc33
Alex Shortt <alexander.shortt@gmail.com>
2020-08-31 00:22:27 -0700

    gallery v10, tweak main room

--- src/models/SpacesGallery.tsx
+++ src/models/SpacesGallery.tsx
@@ -13,10 +13,11 @@ import { useConvexPolyhedron } from "use-cannon";
 
 type GLTFResult = GLTF & {
   nodes: {
-    GALLERYMESH: THREE.Mesh;
-  };
-  materials: {
-    INSIDE: THREE.MeshStandardMaterial;
+    GALLERYFULL: THREE.Mesh;
+    ROOFLIP2: THREE.Mesh;
+    GALLERYWALLS: THREE.Mesh;
+    ROOFLIP1: THREE.Mesh;
+    GALLERYFLOOR: THREE.Mesh;
   };
 };
 
@@ -24,34 +25,52 @@ export default function Model(props: ModelProps) {
   const { useEnvStore } = props;
   const setLoading = useEnvStore((st) => st.setLoading);
   const group = useRef<THREE.Group>();
-  const { nodes, materials } = useLoader<GLTFResult>(
+  const { nodes } = useLoader<GLTFResult>(
     GLTFLoader,
-    "https://d27rt3a60hh1lx.cloudfront.net/models/SpacesGallery5/SpacesGallery5.gltf",
+    "https://d27rt3a60hh1lx.cloudfront.net/models/SpacesGallery10/SpacesGallery10.glb",
     loadModel(setLoading)
   );
-  materials["INSIDE"].metalness = 0.4;
-  // materials["INSIDE"].refractionRatio = 0.3;
-  materials["INSIDE"].side = THREE.DoubleSide;
 
-  // const galleryGeo = useMemo(() => {
-  //   return new THREE.Geometry().fromBufferGeometry(
-  //     nodes.GALLERYMESH.geometry as BufferGeometry
-  //   );
-  // }, [nodes]);
-  // const [wallsHitbox] = useConvexPolyhedron(() => ({
-  //   type: "Static",
-  //   args: galleryGeo
-  //     .clone()
-  //     .scale(100, 100, 100)
-  //     .translate(0, 0.5, 0),
-  // }));
+  // materials["INSIDE"].metalness = 0.4;
+  // // materials["INSIDE"].refractionRatio = 0.3;
+  // materials["INSIDE"].side = THREE.DoubleSide;
+
+  const floorGeo = useMemo(() => {
+    return new THREE.Geometry().fromBufferGeometry(
+      nodes.GALLERYFLOOR.geometry as BufferGeometry
+    );
+  }, [nodes]);
+  const [floorHitbox] = useConvexPolyhedron(() => ({
+    type: "Static",
+    args: floorGeo.clone().scale(100, 100, 100).translate(0, 0.5, 0),
+  }));
+
+  const wallsGeo = useMemo(() => {
+    return new THREE.Geometry().fromBufferGeometry(
+      nodes.GALLERYWALLS.geometry as BufferGeometry
+    );
+  }, [nodes]);
+  const [wallsHitbox] = useConvexPolyhedron(() => ({
+    type: "Static",
+    args: wallsGeo.clone().scale(100, 100, 100).translate(0, 0.5, 0),
+  }));
 
   return (
     <group ref={group} {...props} dispose={null}>
       <group position={[0, 0.5, 0]} scale={[100, 100, 100]}>
         <mesh
-          material={materials.INSIDE}
-          geometry={nodes.GALLERYMESH.geometry}
+          material={nodes.GALLERYFULL.material}
+          geometry={nodes.GALLERYFULL.geometry}
+        />
+        <mesh
+          material={nodes.ROOFLIP2.material}
+          geometry={nodes.ROOFLIP2.geometry}
+          position={[0, 0.070056, 0]}
+        />
+        <mesh
+          material={nodes.ROOFLIP1.material}
+          geometry={nodes.ROOFLIP1.geometry}
+          position={[0, 0.070056, 0]}
         />
       </group>
     </group>
--- src/scenes/Outside/components/MainRoom.tsx
+++ src/scenes/Outside/components/MainRoom.tsx
@@ -3,12 +3,12 @@ import FramedImage from "three-components/FramedImage";
 
 const MainRoom = () => {
   return (
-    <group>
+    <group position={[20, 2.5, 20]}>
       <FramedImage
         src="https://d27rt3a60hh1lx.cloudfront.net/content/opening/robert/cottoncandy.jpg"
         ratio={[1, 1]}
         sizeScale={5}
-        position={[4, 2, 0]}
+        position={[4, 2.5, 0]}
         floating
       />
     </group>
