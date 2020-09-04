6bff8aec7c3ade8c088a27789005a465dabfa6d7
Alex Shortt <alexander.shortt@gmail.com>
2020-09-04 12:26:28 -0700

    set up chad page basics

---
+++ src/scenes/Chad/index.tsx
@@ -0,0 +1,59 @@
+import React, { Suspense, useState } from "react";
+import { Physics } from "use-cannon";
+import { Canvas } from "react-three-fiber";
+import InfinitePlane from "three-components/InfinitePlane";
+import Player from "core/Player";
+import { SceneComponent } from "types/scene";
+
+import Analytics from "ui-components/Analytics";
+import HDRISky from "three-components/HDRI/HDRISky";
+import Effects from "core/Effects";
+import SpacesSphere from "models/SpacesSphere";
+import { WebGLRenderer } from "three";
+
+const physicsProps = {
+  iterations: 20,
+  size: 10,
+  allowSleep: false,
+  gravity: [0, -30, 0],
+  defaultContactMaterial: {
+    friction: 0,
+    // restitution?: number
+    // contactEquationStiffness?: number
+    // contactEquationRelaxation?: number
+    // frictionEquationStiffness?: number
+    // frictionEquationRelaxation?: number
+  },
+};
+
+const Chad: SceneComponent = (props) => {
+  const { useEnvStore, defaultCanvasProps, children } = props;
+
+  const [renderer, setRenderer] = useState<WebGLRenderer>();
+
+  return (
+    <>
+      <Analytics />
+      <Canvas
+        {...defaultCanvasProps}
+        onCreated={(props) => {
+          setRenderer(props.gl);
+        }}
+      >
+        {children}
+        <Physics {...physicsProps}>
+          <HDRISky />
+          <InfinitePlane height={-0.001} />
+          <Player useEnvStore={useEnvStore} />
+          <Suspense fallback={null}>
+            <SpacesSphere useEnvStore={useEnvStore} />
+          </Suspense>
+          <ambientLight intensity={0.3} />
+          <Effects renderer={renderer} />
+        </Physics>
+      </Canvas>
+    </>
+  );
+};
+
+export default Chad;
