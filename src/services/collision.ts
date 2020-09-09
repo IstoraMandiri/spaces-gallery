c37df58a6186c8289c40306ddf231bd5791f591f
Alex Shortt <alexander.shortt@gmail.com>
2020-09-08 21:49:05 -0700

    trimesh, convex

--- src/services/collision.ts
+++ src/services/collision.ts
@@ -1,9 +1,23 @@
 import { useMemo } from "react";
 import * as THREE from "three";
 import { BufferGeometry } from "three";
-import { useConvexPolyhedron } from "use-cannon";
+import { useConvexPolyhedron, useTrimesh } from "use-cannon";
 
-export const useCollision = (geometry: BufferGeometry) => {
+export const useTrimeshCollision = (geometry: BufferGeometry) => {
+  const vertices = geometry.attributes.position.array;
+  // @ts-ignore
+  const indices = geometry.index.array;
+
+  // @ts-ignore
+  const [hitbox] = useTrimesh(() => ({
+    type: "Static",
+    args: [vertices, indices],
+  }));
+
+  return hitbox;
+};
+
+export const useConvexCollision = (geometry: BufferGeometry) => {
   const geo = useMemo(() => new THREE.Geometry().fromBufferGeometry(geometry), [
     geometry,
   ]);
