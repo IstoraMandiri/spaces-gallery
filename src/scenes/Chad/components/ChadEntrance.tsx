bbd8479da59fa2ba568382b0f3a1df7f0948b9d0
Alex Shortt <alexander.shortt@gmail.com>
2020-09-10 21:10:26 -0700

    add to entrance

--- src/scenes/Chad/components/ChadEntrance.tsx
+++ src/scenes/Chad/components/ChadEntrance.tsx
@@ -1,42 +1,45 @@
 import React, { useMemo } from "react";
-import { SpotLight } from "three";
+import { Color, SpotLight } from "three";
 import { EnvironmentStoreHook } from "stores/environment";
 import { Text } from "drei";
 import Logo from "three-components/Logo";
+import FramedImage from "three-components/FramedImage";
+
+const FRAME_COLOR = new Color(0xffffff);
 
 const ChadEntrance = (props: { useEnvStore: EnvironmentStoreHook }) => {
   const { useEnvStore } = props;
 
-  const light1 = useMemo(() => new SpotLight(), []);
-
-  const topLight = {
-    distance: 20,
-    color: 0xffffff,
-    intensity: 1.3,
-    angle: Math.PI / 2.8,
-    penumbra: 0.9,
-  };
-
   return (
     <group position={[-2, -2, 65]}>
-      <group position={[-2, 2, -6]}>
-        <primitive object={light1} {...topLight} />
-        <primitive object={light1.target} position={[0, -100, 0]} />
-      </group>
+      <pointLight position={[-2, 2, -6]} intensity={0.15} />
       <group
         name="wall"
-        rotation={[0, 0, 0]}
-        position={[-1, 0.25, -8]}
-        scale={[0.7, 0.7, 0.7]}
+        rotation={[0, Math.PI, 0]}
+        position={[-5, 0.5, 4]}
+        scale={[2, 2, 2]}
       >
         <Text scale={[6, 6, 6]} position={[-4, 0, 0]} textAlign="left">
           CHAD KNIGHT
         </Text>
-        <Text scale={[4, 4, 4]} position={[-1.6, 0, 0]}>
+        <Text scale={[3.5, 3.5, 3.5]} position={[-1.6, 0, 0]}>
           x
         </Text>
-        <group position={[-2.75, -1.45, 0]}>
+        <Logo
+          useEnvStore={useEnvStore}
+          floating
+          rotation={[0, Math.PI / 2, 0]}
+        />
+      </group>
+      <group
+        name="left-wall"
+        rotation={[0, Math.PI / 2, 0]}
+        position={[-7, 0.25, -7]}
+        scale={[0.7, 0.7, 0.7]}
+      >
+        <group position={[-2.75, -1.75, 0]}>
           <Text
+            position={[0, -0.2, 0]}
             scale={[2, 2, 2]}
             anchorY="middle"
             maxWidth={3}
@@ -50,16 +53,75 @@ const ChadEntrance = (props: { useEnvStore: EnvironmentStoreHook }) => {
           exoskeleton humans inhabit to enter the virtual world."
             }
           </Text>
+          <Text
+            position={[-1.8, 0.5, 0]}
+            scale={[3, 3, 3]}
+            anchorY="middle"
+            maxWidth={3}
+            textAlign="left"
+            color="black"
+          >
+            {"VIRTUAL GENESIS"}
+          </Text>
           <mesh position={[0, 0, -0.25 / 2 - 0.001]}>
             <boxBufferGeometry args={[7, 2, 0.25]} attach="geometry" />
             <meshStandardMaterial color="white" attach="material" />
           </mesh>
         </group>
-        <Logo
-          useEnvStore={useEnvStore}
-          floating
-          rotation={[0, Math.PI / 2, 0]}
-        />
+      </group>
+      <group
+        name="right-wall"
+        rotation={[0, -Math.PI / 2, 0]}
+        position={[4.3, 0, -7]}
+        scale={[0.7, 0.7, 0.7]}
+      >
+        <group position={[2.75, -0.25, 0]}>
+          <FramedImage
+            sizeScale={3}
+            ratio={[1, 1]}
+            src={
+              "https://d27rt3a60hh1lx.cloudfront.net/content/chadknight/harris/chaptsikc.jpg"
+            }
+            floating
+            color={FRAME_COLOR}
+            position={[-3.5, 0, 0]}
+          />
+          <FramedImage
+            sizeScale={3}
+            ratio={[1, 1]}
+            src={
+              "https://d27rt3a60hh1lx.cloudfront.net/content/chadknight/harris/fishing.jpg"
+            }
+            floating
+            color={FRAME_COLOR}
+          />
+          <FramedImage
+            sizeScale={3}
+            ratio={[1, 1]}
+            src={
+              "https://d27rt3a60hh1lx.cloudfront.net/content/chadknight/harris/september.jpg"
+            }
+            floating
+            color={FRAME_COLOR}
+            position={[3.5, 0, 0]}
+          />
+        </group>
+        <group position={[2.5, -2, 0.75]} scale={[0.4, 0.4, 0.4]}>
+          <Text
+            position={[0, 0, 0]}
+            scale={[3, 3, 3]}
+            anchorY="middle"
+            maxWidth={3}
+            textAlign="left"
+            color="black"
+          >
+            {"Music provided by Harris Cole"}
+          </Text>
+          <mesh position={[0, 0, -0.25 / 2 - 0.001]}>
+            <boxBufferGeometry args={[7, 1.5, 0.25]} attach="geometry" />
+            <meshStandardMaterial color="white" attach="material" />
+          </mesh>
+        </group>
       </group>
     </group>
   );
