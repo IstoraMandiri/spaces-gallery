109c4f98fe2e84e347041f26fb8b4312ad9f6cab
EvanDElia <deliaevan1@gmail.com>
2020-09-17 18:03:04 -0400

    shirts - empty world with music

---
+++ src/scenes/Shirts/index.tsx
@@ -0,0 +1,57 @@
+import React, { Suspense, useMemo, useState } from "react";
+import { Physics } from "use-cannon";
+import { Canvas, useFrame } from "react-three-fiber";
+import InfinitePlane from "three-components/InfinitePlane";
+import Player from "core/Player";
+import { SceneComponent } from "types/scene";
+import SpacesSphere from "models/SpacesSphere";
+import ShirtsMusic from "./components/ShirtsMusic";
+
+import Analytics from "ui-components/Analytics";
+import HDRISky from "three-components/HDRI/HDRISky";
+import { Sky } from "drei";
+
+const physicsProps = {
+  iterations: 20,
+  size: 10,
+  allowSleep: false,
+  gravity: [0, -30, 0],
+  defaultContactMaterial: {
+    friction: 0,
+    // restitution?: number
+    // contactEquationStiffness?: number
+    // contactEquationRelaxation?: number
+    // frictionEquationStiffness?: number
+    // frictionEquationRelaxation?: number
+  },
+};
+
+const Multiplayer: SceneComponent = (props) => {
+  const { useEnvStore, defaultCanvasProps, children } = props;
+
+  return (
+    <>
+      <Analytics />
+      <Canvas {...defaultCanvasProps}>
+        {children}
+        <Physics {...physicsProps}>
+          <Sky />
+          <InfinitePlane height={-0.001} />
+          <Player useEnvStore={useEnvStore} />
+          <ambientLight intensity={1} />
+          <group position={[0, 0, 23]}>
+            <ShirtsMusic
+              useEnvStore={useEnvStore}
+              url="https://spaces-gallery-assets.s3-us-west-1.amazonaws.com/audio/harris+cole+mix.mp3"
+            />
+          </group>
+          <Suspense fallback={null}>
+            <SpacesSphere useEnvStore={useEnvStore} />
+          </Suspense>
+        </Physics>
+      </Canvas>
+    </>
+  );
+};
+
+export default Multiplayer;
