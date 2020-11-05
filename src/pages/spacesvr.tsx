9efa2115591c858728122e374cc752a03d40f560
Alex Shortt <alexander.shortt@gmail.com>
2020-11-05 15:55:35 -0800

    throw down ho down - scene w/ first glb

---
+++ src/pages/spacesvr.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { NextPage } from "next";
+import Head from "next/head";
+import dynamic from "next/dynamic";
+
+const Environment = dynamic(
+  import("@spacesvr/core/environments/PlayerEnvironment"),
+  {
+    ssr: false,
+  }
+);
+const SpacesVR = dynamic(import("scenes/SpacesVR"), { ssr: false });
+
+const SpacesVRPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Spaces Gallery</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,300&family=Space+Mono&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Environment scene={SpacesVR} />
+    </>
+  );
+};
+
+export default SpacesVRPage;
---
+++ src/scenes/SpacesVR/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Physics } from "@react-three/cannon";
+import { Canvas } from "react-three-fiber";
+import InfinitePlane from "@spacesvr/components/InfinitePlane";
+import Player from "@spacesvr/core/players/Player";
+import { SceneComponent } from "@spacesvr/core/types/scene";
+
+import { useAnalytics } from "services/analytics";
+import RealisticEffects from "@spacesvr/core/effects/RealisticEffects";
+import Logo from "@spacesvr/components/Logo";
+import { Sky } from "@react-three/drei";
+import SpacesVRElements from "./models/SpacesVRElements";
+
+const physicsProps = {
+  iterations: 20,
+  size: 10,
+  allowSleep: false,
+  gravity: [0, -30, 0],
+  defaultContactMaterial: {
+    friction: 0,
+  },
+};
+
+const SpacesVR: SceneComponent = (props) => {
+  const { useEnvStore, defaultCanvasProps, children } = props;
+
+  useAnalytics();
+
+  return (
+    <Canvas {...defaultCanvasProps}>
+      {children}
+      <Physics {...physicsProps}>
+        <Logo />
+        <Sky />
+        <InfinitePlane height={-0.001} />
+        <SpacesVRElements />
+        <Player useEnvStore={useEnvStore} initPos={[0, 1, 2]} />
+        <ambientLight intensity={1} />
+        <pointLight intensity={1} color={"white"} distance={55} />
+        <RealisticEffects />
+      </Physics>
+    </Canvas>
+  );
+};
+
+export default SpacesVR;
